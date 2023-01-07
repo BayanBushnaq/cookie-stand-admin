9e17a87d813b16fa5b337394331a4506ec637768
BayanBushnaq <bayanmohdali@yahoo.com>
2023-01-07 22:00:48 +0000

    feat : add anew feature to change the theme for page

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function Footer() {
     return(
-        <footer className='py-6 text-center'>
+        <footer className='py-6 text-center  dark:bg-stone-400'>
             <p className='text-gray-500 font-extralight text-center mt-40'> &copy; 2022 </p>
         </footer>
     )
--- app/component/Header.js
+++ app/component/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
+import { useContext } from 'react';
+import { ThemeContext } from '../../contexts/theme';
+
+
 export default function Header() {
+    const {isDarkTheme,toggleThemeHandler} = useContext(ThemeContext);
+    console.log(ThemeContext)
+
     return(
-        <header >
-            <nav className='flex flex-wrap items-center px-3 bg-teal-400'>
+        <header  >
+            <nav className='flex flex-wrap items-center px-3 bg-teal-400  dark:bg-stone-500'>
             <Link className='inline-flex items-center px-2 mr-4' href='/'>
             <Image
                 src="/assets/cookie-stand-admin-app.png"
@@ -14,6 +21,9 @@ export default function Header() {
             />
             <span className='text-xl font-bold tracking-wide text-white uppercase'>Cookie Stand Admin</span>
             </Link>
+            <button onClick={toggleThemeHandler} type="submit" className='bg-stone-300 text-base bg-contain bg-origin-content
+     text-teal-900 rounded-md hover:bg-orange-200 px-2 py-1 font-serif font-bold absolute top-3 
+     left-2/4 ml-80 '>Theme</button> 
             </nav>
         </header>
     )
